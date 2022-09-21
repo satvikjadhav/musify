56276ee7c6ac9993ee504a471b89a264ec0889fc
Satvik Jadhav <satvikj5@gmail.com>
2022-09-21 23:53:20 +0530

    adding task_functions_template python file

---
+++ airflow/dags/task_functions_template.py
@@ -0,0 +1,20 @@
+from airflow.providers.google.cloud.operators.bigquery import (BigQueryCreateExternalTableOperator,
+                                                               BigQueryCreateEmptyTableOperator,
+                                                               BigQueryInsertJobOperator,
+                                                               BigQueryDeleteTableOperator)
+
+
+def create_external_table():
+    ...
+
+
+def create_empty_table():
+    ...
+
+
+def insert_job():
+    ...
+
+
+def delete_external_table():
+    ...
