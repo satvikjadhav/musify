fa349b1c2a5e7b3c7f6b484eab2c02af3e8cbcdc
Satvik Jadhav <satvikj5@gmail.com>
2022-09-24 16:38:03 +0530

    completing musify dag file

--- airflow/dags/musify_dag.py
+++ airflow/dags/musify_dag.py
@@ -20,7 +20,7 @@ BIGQUERY_DATASET = os.environ.get('BIGQUERY_DATASET', 'musify_stg')
 
 EXECUTION_MONTH = '{{ logical_date.strftime("%-m") }}'
 EXECUTION_DAY = '{{ logical_date.strftime("%-d") }}'
-EXECUTION_HOUR = '{{ logical_date.strftime("%-H") }}'
+EXECUTION_YEAR = '{{ logical_date.strftime("%Y") }}'
 EXECUTION_DATETIME_STR = '{{ logical_date.strftime("%m%d%H") }}'
 
 TABLE_MAP = { f"{event.upper()}_TABLE" : event for event in EVENTS}
@@ -35,3 +35,61 @@ MACRO_VARS.update(TABLE_MAP)
 default_args = {
     'owner' : 'satvik'
 }
+
+with DAG(
+    dag_id = f'musify_dag',
+    default_args = default_args,
+    description = f'Hourly data pipeline to generate dims and facts for musify',
+    schedule_interval="5 * * * *", #At the 5th minute of every hour
+    start_date=datetime(2022,3,26,18),
+    catchup=False,
+    max_active_runs=1,
+    user_defined_macros=MACRO_VARS,
+    tags=['musify']
+) as dag:
+    
+    initate_dbt_task = BashOperator(
+        task_id = 'dbt_initiate',
+        bash_command = 'cd /dbt && dbt deps && dbt seed --select state_codes --profiles-dir . --target prod'
+    )
+
+    execute_dbt_task = BashOperator(
+        task_id = 'dbt_musify_run',
+        bash_command = 'cd /dbt && dbt deps && dbt run --profiles-dir . --target prod'
+    )
+
+    for event in EVENTS:
+        
+        staging_table_name = event
+        # extra {} for f-strings escape
+        insert_query = f"{{% include 'sql/{event}.sql' %}}" 
+        external_table_name = f'{staging_table_name}_{EXECUTION_DATETIME_STR}'
+        events_data_path = f'{staging_table_name}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/year={EXECUTION_YEAR}'
+        events_schema = schema[event]
+
+        create_external_table_task = create_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name, 
+                                                           GCP_GCS_BUCKET, 
+                                                           events_data_path)
+
+        create_empty_table_task = create_empty_table(event,
+                                                     GCP_PROJECT_ID,
+                                                     BIGQUERY_DATASET,
+                                                     staging_table_name,
+                                                     events_schema)
+                                                
+        execute_insert_query_task = insert_job(event,
+                                               insert_query,
+                                               BIGQUERY_DATASET,
+                                               GCP_PROJECT_ID)
+
+        delete_external_table_task = delete_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name)
+                    
+        
+        create_external_table_task >> create_empty_table_task >> execute_insert_query_task >> \
+        delete_external_table_task >> initate_dbt_task >> execute_dbt_task
